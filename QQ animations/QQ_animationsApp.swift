27a9cf2ef98361072e9679209d1d71c6ae3b183b
Rhett Wilhoit <rawville@gmail.com>
2025-03-28 08:35:05 -0400

    Initial Commit

---
+++ QQ animations/QQ_animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QQ_animationsApp.swift
+//  QQ animations
+//
+//  Created by Rhett Wilhoit on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct QQ_animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QQ animationsTests/QQ_animationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  QQ_animationsTests.swift
+//  QQ animationsTests
+//
+//  Created by Rhett Wilhoit on 3/28/25.
+//
+
+import Testing
+@testable import QQ_animations
+
+struct QQ_animationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
